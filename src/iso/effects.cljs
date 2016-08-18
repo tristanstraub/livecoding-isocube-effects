036069b794893ceae6d60c070969f7b097ce0386
Tristan Straub <tristanstraub@gmail.com>
2016-08-18 22:23:55 +1000

    Weird wavy shape thing

--- src/iso/effects.cljs
+++ src/iso/effects.cljs
@@ -17,15 +17,20 @@
 
 (defn draw-circular [ctx width height delta]
   (let [d (/ delta 1000)]
-    (let [n 20]
-      (doseq [[pos i] (map (fn [i] [[(* 200 (Math/cos (+ d (* i (/ (* 2 Math/PI) n)))))
-                                     (* 200 (Math/sin (+ d (* i (/ (* 2 Math/PI) n)))))]
+    (let [growth 4
+          n (* 20 growth)
+          R 200]
+      (doseq [[pos i] (map (fn [i] [[(* (+ R (* 480 (Math/cos (* i (/ (* 2 Math/PI) n)))))
+                                        (Math/cos (+ d (* i (/ (* 2 Math/PI) n)))))
+                                     (* (+ R (* 120 (Math/sin (* i (/ (* 2 Math/PI) n)))))
+                                        (Math/sin (+ d (* i (/ (* 2 Math/PI) n)))))]
                                     i])
                            (range n))]
 
         (d/draw-iso-cube ctx (d/v-+-v [(/ width 2) (/ height 2)] pos)
                          :color (str "#"
-                                     (nth "0123456789abcdef" (brighten (mod (/ (* 16 (* 3 i)) n) 16)))
-                                     (nth "0123456789abcdef" (brighten (mod (/ (* 16 i) n) 16)))
-                                     (nth "0123456789abcdef" (brighten (mod (/ (* 16 (* 7 i)) n) 16))))
-                         :scale 0.25)))))
+                                     (nth "0123456789abcdef" (mod (* 15 (Math/sin (* i (/ (/ Math/PI 2) n)))) 16))
+                                     (nth "0123456789abcdef" (mod (* 15 (Math/sin (* 3 i (/ (/ Math/PI 2) n)))) 16))
+                                     (nth "0123456789abcdef" (mod (* 15 (Math/sin (* 7 i (/ (/ Math/PI 2) n)))) 16))
+                                     )
+                         :scale (/ 0.25 growth))))))
